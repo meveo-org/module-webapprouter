73337a06b5c4e0614703e985aac25a43bd2e2827
meveo.admin <no-mail@meveo.com>
2022-03-04 11:38:33 +0000

    Add JS script for Endpoint: webapp

--- facets/javascript/endpoints/webapp.js
+++ facets/javascript/endpoints/webapp.js
@@ -1,53 +1,33 @@
-import EndpointInterface from "#{API_BASE_URL}/api/rest/endpoint/EndpointInterface.js";
-
-// the request schema, this should be updated
-// whenever changes to the endpoint parameters are made
-// this is important because this is used to validate and parse the request parameters
-const requestSchema = {
-  "title" : "webappRequest",
-  "id" : "webappRequest",
-  "default" : "Schema definition for webapp",
-  "$schema" : "http://json-schema.org/draft-07/schema",
-  "type" : "object"
+const webapp = async (parameters) =>  {
+	const baseUrl = window.location.origin;
+	const url = new URL(`${window.location.pathname.split('/')[1]}/rest/webapp/${parameters.appCode}`, baseUrl);
+	return fetch(url.toString(), {
+		method: 'GET'
+	});
 }
 
-// the response schema, this should be updated
-// whenever changes to the endpoint parameters are made
-// this is important because this could be used to parse the result
-const responseSchema = {
-  "title" : "webappResponse",
-  "id" : "webappResponse",
-  "default" : "Schema definition for webapp",
-  "$schema" : "http://json-schema.org/draft-07/schema",
-  "type" : "object",
-  "properties" : {
-    "result" : {
-      "title" : "result",
-      "type" : "string",
-      "minLength" : 1
-    }
-  }
+const webappForm = (container) => {
+	const html = `<form id='webapp-form'>
+		<div id='webapp-appCode-form-field'>
+			<label for='appCode'>appCode</label>
+			<input type='text' id='webapp-appCode-param' name='appCode'/>
+		</div>
+		<button type='button'>Test</button>
+	</form>`;
+
+	container.insertAdjacentHTML('beforeend', html)
+
+	const appCode = container.querySelector('#webapp-appCode-param');
+
+	container.querySelector('#webapp-form button').onclick = () => {
+		const params = {
+			appCode : appCode.value !== "" ? appCode.value : undefined
+		};
+
+		webapp(params).then(r => r.text().then(
+				t => alert(t)
+			));
+	};
 }
 
-// should contain offline mock data, make sure it adheres to the response schema
-const mockResult = {};
-
-class webapp extends EndpointInterface {
-	constructor() {
-		// name and http method, these are inserted when code is generated
-		super("webapp", "GET");
-		this.requestSchema = requestSchema;
-		this.responseSchema = responseSchema;
-		this.mockResult = mockResult;
-	}
-
-	getRequestSchema() {
-		return this.requestSchema;
-	}
-
-	getResponseSchema() {
-		return this.responseSchema;
-	}
-}
-
-export default new webapp();
+export { webapp, webappForm };
