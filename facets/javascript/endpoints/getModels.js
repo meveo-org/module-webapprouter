0a86482258d42995a304e3f092ee39592ae961e4
meveo.admin <no-mail@meveo.com>
2022-03-07 10:46:26 +0000

    Add JS script for Endpoint: getModels

--- facets/javascript/endpoints/getModels.js
+++ facets/javascript/endpoints/getModels.js
@@ -1,53 +1,33 @@
-import EndpointInterface from "#{API_BASE_URL}/api/rest/endpoint/EndpointInterface.js";
-
-// the request schema, this should be updated
-// whenever changes to the endpoint parameters are made
-// this is important because this is used to validate and parse the request parameters
-const requestSchema = {
-  "title" : "getModelsRequest",
-  "id" : "getModelsRequest",
-  "default" : "Schema definition for getModels",
-  "$schema" : "http://json-schema.org/draft-07/schema",
-  "type" : "object"
+const getModels = async (parameters) =>  {
+	const baseUrl = window.location.origin;
+	const url = new URL(`${window.location.pathname.split('/')[1]}/rest/getModels/${parameters.moduleCode}`, baseUrl);
+	return fetch(url.toString(), {
+		method: 'GET'
+	});
 }
 
-// the response schema, this should be updated
-// whenever changes to the endpoint parameters are made
-// this is important because this could be used to parse the result
-const responseSchema = {
-  "title" : "getModelsResponse",
-  "id" : "getModelsResponse",
-  "default" : "Schema definition for getModels",
-  "$schema" : "http://json-schema.org/draft-07/schema",
-  "type" : "object",
-  "properties" : {
-    "result" : {
-      "title" : "result",
-      "type" : "string",
-      "minLength" : 1
-    }
-  }
+const getModelsForm = (container) => {
+	const html = `<form id='getModels-form'>
+		<div id='getModels-moduleCode-form-field'>
+			<label for='moduleCode'>moduleCode</label>
+			<input type='text' id='getModels-moduleCode-param' name='moduleCode'/>
+		</div>
+		<button type='button'>Test</button>
+	</form>`;
+
+	container.insertAdjacentHTML('beforeend', html)
+
+	const moduleCode = container.querySelector('#getModels-moduleCode-param');
+
+	container.querySelector('#getModels-form button').onclick = () => {
+		const params = {
+			moduleCode : moduleCode.value !== "" ? moduleCode.value : undefined
+		};
+
+		getModels(params).then(r => r.text().then(
+				t => alert(t)
+			));
+	};
 }
 
-// should contain offline mock data, make sure it adheres to the response schema
-const mockResult = {};
-
-class getModels extends EndpointInterface {
-	constructor() {
-		// name and http method, these are inserted when code is generated
-		super("getModels", "GET");
-		this.requestSchema = requestSchema;
-		this.responseSchema = responseSchema;
-		this.mockResult = mockResult;
-	}
-
-	getRequestSchema() {
-		return this.requestSchema;
-	}
-
-	getResponseSchema() {
-		return this.responseSchema;
-	}
-}
-
-export default new getModels();
+export { getModels, getModelsForm };
